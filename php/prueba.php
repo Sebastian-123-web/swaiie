34ab80ae628b8991d598d29518eef9aaa83761e1
Sebastian-123-web <sebastian09_26@hotmail.com>
2020-12-21 17:21:55 -0500

    .

--- php/prueba.php
+++ php/prueba.php
@@ -1,10 +1,9 @@
 <?php
   include '../class/database.php';
   //include 'conexion.php';
-  $conexion = new DataBase();
-  $conexion->ConexionDB();
+  $conexi = new DataBase();
   $sql = "INSERT INTO `antivirus` (`id_antivirus`, `antivirus`) VALUES (NULL, 'Gdata')";
-  $query = mysqli_query($conexion, $sql);
+  $query = mysqli_query($conexi, $sql);
   //$array = mysqli_fetch_array($query);
   //echo $array[0];
 ?>
