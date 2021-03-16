08177f0cda84492768d51635bf913373c5ffe073
Sebastian-123-web <Rb1237894560@>
2021-03-16 18:14:56 -0500

    .

--- clases/EquipoComputo.php
+++ clases/EquipoComputo.php
@@ -1,16 +1,12 @@
 <?php
   class EquipoComputo{
 
-    function __construct(){
-    }
-
-    
     //  AGREGAR EQUIPO A LA BASE DE DATOS
-    public function AgregarEquipo($id_equipo,$nom_equipo,$ram,$disco,$id_mm,$id_os,$id_cpu,$id_tipo,$id_antivirus,$id_software,$mantenimiento,$id_user){
+    public function AgregarEquipo($id_equipo,$nom_equipo,$ram,$disco,$tipo_disco,$id_mm,$id_os,$id_cpu,$generacion,$id_tipo,$id_antivirus,$id_software,$estado,$mantenimiento,$id_user){
       include 'conexion.php';
       $sql = "
-        INSERT INTO `equipo` (`id_equipo`, `nom_equipo`, `ram`, `disco`, `id_mm`, `id_os`, `id_cpu`, `id_tipo`, `id_antivirus`, `id_software`, `mantenimiento`, `id_user`)
-        VALUES ('$id_equipo', '$nom_equipo', '$ram', '$disco', '$id_mm', '$id_os', '$id_cpu', '$id_tipo', '$id_antivirus', '$id_software', '$mantenimiento', '$id_user');
+        INSERT INTO `equipo` (`id_equipo`, `nom_equipo`, `ram`, `disco`, `tipo_disco`, `id_mm`, `id_os`, `id_cpu`, `generacion`, `id_tipo`, `id_antivirus`, `id_software`, `estado`, `mantenimiento`, `id_user`)
+        VALUES ('$id_equipo', '$nom_equipo', '$ram', '$disco', '$tipo_disco', '$id_mm', '$id_os', '$id_cpu','$generacion', '$id_tipo', '$id_antivirus', '$id_software', '$estado', '$mantenimiento', '$id_user');
       ";
       mysqli_query($link, $sql);
     }
@@ -20,7 +16,7 @@
     public function MostrarEquipo(){
       include 'conexion.php';
       $sql = "
-        SELECT `equipo`.`id_equipo`,`equipo`.`nom_equipo`,`equipo`.`ram`,`equipo`.`disco`, `marca_modelo`.`mm`, `os`.`os`, `cpu`.`cpu`, `tipo`.`tipo`, `antivirus`.`antivirus`, `software`.`software`, `equipo`.`mantenimiento`, `usuario`.`id_user`
+        SELECT `equipo`.`id_equipo`,`equipo`.`nom_equipo`,`equipo`.`ram`,`equipo`.`disco`,`equipo`.`tipo_disco`, `marca_modelo`.`mm`, `os`.`os`, `cpu`.`cpu`, `equipo`.`generacion`, `tipo`.`tipo`, `antivirus`.`antivirus`, `software`.`software`, `equipo`.`estado`, `equipo`.`mantenimiento`, `usuario`.`id_user`
         FROM `equipo`
         INNER JOIN `marca_modelo` ON `equipo`.`id_mm`=`marca_modelo`.`id_mm`
         INNER JOIN `os` ON `equipo`.`id_os`=`os`.`id_os`
@@ -38,12 +34,15 @@
           'nom_equipo' => $row['nom_equipo'],
           'ram' => $row['ram'],
           'disco' => $row['disco'],
+          'tipo_disco' => $row['tipo_disco'],
           'mm' => $row['mm'],
           'os' => $row['os'],
           'cpu' => $row['cpu'],
+          'generacion' => $row['generacion'],
           'tipo' => $row['tipo'],
           'antivirus' => $row['antivirus'],
           'software' => $row['software'],
+          'estado' => $row['estado'],
           'mantenimiento' => $row['mantenimiento'],
           'id_user' => $row['id_user']
         );
@@ -54,11 +53,11 @@
 
 
     //  EDITAR EQUIPO DE LA BASE DE DATOS
-    public function EditarEquipo($id_equipo,$id_equipo_new,$nom_equipo,$ram,$disco,$id_mm,$id_os,$id_cpu,$id_tipo,$id_antivirus,$id_software,$mantenimiento,$id_user){
+    public function EditarEquipo($id_equipo,$id_equipo_new,$nom_equipo,$ram,$disco,$tipo_disco,$id_mm,$id_os,$id_cpu,$generacion,$id_tipo,$id_antivirus,$id_software,$estado,$mantenimiento,$id_user){
       include 'conexion.php';
       $sql = "
         UPDATE `equipo`
-        SET `id_equipo`='$id_equipo_new' , `nom_equipo`='$nom_equipo' , `ram`='$ram' , `disco`='$disco' , `id_mm`='$id_mm' , `id_os`='$id_os' , `id_cpu`='$id_cpu' , `id_tipo`='$id_tipo' , `id_antivirus`='$id_antivirus' , `id_software`='$id_software' , `mantenimiento`='$mantenimiento' , `id_user`='$id_user'
+        SET `id_equipo`='$id_equipo_new' , `nom_equipo`='$nom_equipo' , `ram`='$ram' , `disco`='$disco' , `tipo_disco`='$tipo_disco' , `id_mm`='$id_mm' , `id_os`='$id_os' , `id_cpu`='$id_cpu' , `generacion`='$generacion' , `id_tipo`='$id_tipo' , `id_antivirus`='$id_antivirus' , `id_software`='$id_software' , `estado`='$estado' , `mantenimiento`='$mantenimiento' , `id_user`='$id_user'
         WHERE `equipo`.`id_equipo`='$id_equipo'
       ";
       mysqli_query($link, $sql);
@@ -72,9 +71,9 @@
       mysqli_query($link, $sql);
     }
   }
-  //$eq = new EquipoComputo();
+  $eq = new EquipoComputo();
   //$eq->MostrarEquipo();
   //$eq->EliminarEquipo();
-  //$eq->EditarEquipo('GEJN3517','GEJN3517','TBGRGAFSI007','32','2000','2','3','3','2','1','1','Aqui va mantenimiento del equipo claro si es que se a hecho','rbanagasta');
-  //$eq->AgregarEquipo('RFD43D1FD3','TBGRGAFSI001','32','2000','2','3','3','2','1','1','Equipo formateado','nin');
+  //$eq->EditarEquipo('GEJN3517','GEJN3517','TBGRGAFSI007','32','2000','HDD','2','3','3','2Gen','2','1','1','Activo','Aqui va mantenimiento del equipo claro si es que se a hecho','rbanagasta');
+  $eq->AgregarEquipo('RFD43D1FD3','TBGRGAFSI001','32','2000','HDD','2','3','3','3','2','1','1','Error','Equipo formateado','nin');
 ?>
